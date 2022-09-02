97a3cdc9691c3edf8ed8a7c31d364a2ef30b877c
tiagosantosp <tiago_s.p@hotmail.com>
2022-09-02 14:41:01 -0300

    teste de pagina

--- src/App.tsx
+++ src/App.tsx
@@ -12,19 +12,20 @@ import Historico from "./pages/Historico";
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Cadastro />} />
-        <Route path="/confirmacao" element={<Confirmacao />} />
-        <Route path="/home"  element={<Home/>}>
-          <Route path="perfil"  element={<Perfil/>}/>
-          <Route path="upload"  element={<Upload/>}/>
-          <Route path="instituicoes"  element={<Instituicoes/>}/>
-          <Route path="historico"  element={<Historico/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Login/>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Login />} />
+    //     <Route path="/register" element={<Cadastro />} />
+    //     <Route path="/confirmacao" element={<Confirmacao />} />
+    //     <Route path="/home"  element={<Home/>}>
+    //       <Route path="perfil"  element={<Perfil/>}/>
+    //       <Route path="upload"  element={<Upload/>}/>
+    //       <Route path="instituicoes"  element={<Instituicoes/>}/>
+    //       <Route path="historico"  element={<Historico/>}/>
+    //     </Route>
+    //   </Routes>
+    // </BrowserRouter>
   )
 }
 
